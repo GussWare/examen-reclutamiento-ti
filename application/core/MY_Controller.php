0b8d9d2536cb8fe1642ac459ebcf532a1a940058
GussWare <gussware@gmail.com>
2022-09-15 16:24:25 -0500

    feat:  se agrego soporte para cors

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,11 +1,14 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed'); 
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class MY_Controller extends CI_Controller {
-
-
+class MY_Controller extends CI_Controller
+{
     public function __construct()
     {
         parent::__construct();
+
+        header('Access-Control-Allow-Origin: *'); //for allow any domain, insecure
+        header('Access-Control-Allow-Headers: *'); //for allow any headers, insecure
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE'); //method allowed
     }
 }
